a2406fe9ef12d4e67a1f8a7f1971f08515abfc10
fabr404 <fabriciofelixsilva11@gmail.com>
2024-09-04 15:39:58 -0300

    atv 03

---
+++ atv_03.py
@@ -0,0 +1,5 @@
+# Crie um programa que calcule a área de um retângulo, pedindo ao usuário a largura e a altura.
+l = float(input("largura do retangulo : "))
+c = float(input("comprimento do retangulo : "))
+s = l*c
+print(s)
